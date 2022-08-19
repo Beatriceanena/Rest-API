ec97c005134270ec49c0f4d274f39a9c2be1e897
Beatriceanena <anenabeattrice@gmail.com>
2022-08-19 12:25:08 +0300

    changes

--- index.js
+++ index.js
@@ -1,40 +1,35 @@
 //Questions:
 //1.Show products from /products
-//2.Show fruits from /products/fruits
-//3.Show vegetables from /products/vegetables
 
-fetch('localhost:5000/products',)
- 
+fetch('/products',{
+    method:'GET'
+})
+    
 .then((response) =>{
     return response.json();
 })
 
-.then( (data)=> {
-    })
-
-    .then((data) => {
-      let products = data;
-      for(var a=0; a<data.length; a++){
-      document.getElementById("products").innerHTML += data[i].name +" :" +data[i].category;
-      }
-    })
-
 .catch(function (error){
     console.log(error);
 });   
-  
-
-fetch('localhost:5000/products/fruits',)
+ 
+//2.Show fruits from /products/fruits
+fetch('/products/fruits',{
+    method:'GET'
+})
 .then(function (response){
     return response.json();
-});
+})
 
 .then(function (error){
     console.log(error)
 })
 
 
-fetch('localhost:5000/products/vegetables',
+//3.Show vegetables from /products/vegetables
+fetch('/products/vegetables',{
+    method:'GET'
+}
 )
 .then(function(response){
     return response.json();
